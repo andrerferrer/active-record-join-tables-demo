f1d8c8097b5b953f96a7adba4c7c68b26372547f
andre ferrer <andrerferrer@hotmail.com>
2021-06-08 09:25:57 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,29 @@ meals = [
   }
 ]
 
+puts 'Create some meals'
+
 Restaurant.all.each do |restaurant|
   meals.each do |meal|
     Meal.create! **meal, restaurant: restaurant
   end
 end
 
+puts 'Create one shopping cart'
+
+order = ShoppingCart.new
+# This creates a product in the cart
+ProductCart.create!(meal: Meal.first, shopping_cart: order)
+# This does exactly the same as above with another syntax for the second meal
+order.meals << Meal.second
+# Now, we should be able to check the shopping cart
+p "Shopping cart = "
+p order
+puts "\n\n\n"
+# And to check the products inside
+p "Products in the shopping cart ="
+p order.meals
+puts "\n\n\n"
 
 puts "Let's drink a beer"
 puts %{
